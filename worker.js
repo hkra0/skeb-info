25fc9d6d175ded4a34d40510b7ed16a0a18d8843
HkrA <theaurora@outlook.com>
2025-05-11 14:08:19 +0800

    fix: remove wishlist page fetch

--- worker.js
+++ worker.js
@@ -3,17 +3,15 @@ const perPage = 30;
 
 // Cache for HTML content
 let infoPage = null;
-let wishlistPage = null;
 
 export default {
   async fetch(request, env) {
     // Preload HTML content if not already loaded
     if (infoPage === null || wishlistPage === null) {
       const infoResponse = await fetch(env.PAGE_URL);
-      const wishlistResponse = await fetch(new URL('wishlist', env.PAGE_URL));
       console.log(env.PAGE_URL);
       if (!infoResponse.ok || !wishlistResponse.ok) {
-        console.error('Failed to fetch HTML content:', infoResponse.status, wishlistResponse.status);
+        console.error('Failed to fetch HTML content:', infoResponse.status);
         return new Response('Failed to fetch HTML content', { status: 500 });
       }
       infoPage = await infoResponse.text();
